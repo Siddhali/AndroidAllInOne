3f2316a7becd01e9e35289b92abc3cd2788414e6
SiddhaliTari <siddhalitari06@gmail.com>
2020-04-27 13:30:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mynewapp/Activity/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.mynewapp.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynewapp.R
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/mynewapp/Activity/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mynewapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynewapp.R
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
