845373c90157808015dbf12013b11c14e41d6f77
SiddhaliTari <siddhalitari06@gmail.com>
2020-04-28 18:13:58 +0530

    commit  //check push

--- app/src/main/java/com/example/mynewapp/Activity/SecondActivity.kt
+++ app/src/main/java/com/example/mynewapp/Activity/SecondActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import com.example.mynewapp.R
 
 class SecondActivity : AppCompatActivity() {
- feature/US-001-GoogleSignIn
+
 //check push
 
- master
+ 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
