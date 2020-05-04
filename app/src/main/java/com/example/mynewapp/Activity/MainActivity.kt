b0c476909db924538a77659110f18c6075286348
SiddhaliTari <siddhalitari06@gmail.com>
2020-05-04 18:05:25 +0530

    declare variable

--- app/src/main/java/com/example/mynewapp/Activity/MainActivity.kt
+++ app/src/main/java/com/example/mynewapp/Activity/MainActivity.kt
@@ -3,6 +3,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mynewapp.R
 class MainActivity : AppCompatActivity() {
+    var a=1
     //check push
     //branch no 1
     override fun onCreate(savedInstanceState: Bundle?) {
