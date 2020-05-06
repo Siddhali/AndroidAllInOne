3306cb8a7ef856c48c53351927829095d6b21b8a
SiddhaliTari <siddhalitari06@gmail.com>
2020-05-06 12:51:24 +0530

    lint errors

--- app/src/main/java/com/example/mynewapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/mynewapp/activity/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.example.mynewapp.Activity
+package com.example.mynewapp.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mynewapp.R
 class MainActivity : AppCompatActivity() {
-    var a=1
     //check push
     //branch no 1
     override fun onCreate(savedInstanceState: Bundle?) {
